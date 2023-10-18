a8c4d1ac3781fe71f620cd537d87efef28747b1f
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-10-18 21:33:01 +0100

    first commit

---
+++ todos/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed Puppy", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Water Plants", rowAssigned: "User Two" },
+    { rowNumber: 3, rowDescription: "Make Dinner", rowAssigned: "User One" },
+    {
+      rowNumber: 4,
+      rowDescription: "Charge Phone Battery",
+      rowAssigned: "User One",
+    },
+  ]);
+
+  const addTodo = (assigned, description) => {
+    let rowNumber = 0;
+
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
+
+      
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,  
+      rowAssigned: assigned,
+    };
+
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  return (
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todos</div>
+        <div className="class-body p-3">
+          <TodoTable todos={todos} />
+
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
+
+          <NewTodoForm addTodo={addTodo} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ todos/src/components/NewTodoForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const NewTodoForm = (props) => {
+  const [assigned, setAssigned] = useState("");
+  const [description, setDescription] = useState("");
+
+  const submitTodo = () => {
+    if (assigned.trim !== 0 && description.trim !== 0) {
+      props.addTodo(assigned, description);
+      setAssigned("");
+      setDescription("");
+    }
+  };
+
+  return (
+    <div className="mt-5">
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Assigned</label>
+          <input
+            type="text"
+            onChange={(e) => setAssigned(e.target.value)}
+            className="form-control"
+            value={assigned}
+            required
+          ></input>
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <textarea
+            className="form-control"
+            onChange={(e) => setDescription(e.target.value)}
+            rows={3}
+            value={description}
+            required
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          onClick={submitTodo}
+          className="btn btn-primary mt-3"
+        >
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTodoForm;
